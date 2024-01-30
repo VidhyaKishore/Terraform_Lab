d0012fd26ce9f33a5ed3840b2faa2884657fe042
unknown <labguide@gmail.com>
2024-01-30 22:00:07 +0530

    This is my 1st commit

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {}
+
+resource "aws_instance" "MyEc2" {
+  ami                    = "ami-0a3c3a20c09d6f377"
+  instance_type          = "t2.micro"
+ 
+  tags = {
+    Name = "Hello_world"
+  }
+}
