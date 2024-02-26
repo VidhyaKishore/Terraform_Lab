2187d9f2a655333255f2704b3cbce08951227d16
Vidhyakishore <30649286+VidhyaKishore@users.noreply.github.com>
2024-02-26 14:39:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-region = "us-east-1"
+region = "ap-south-1"
 }
 
 resource "aws_instance" "MyEc2" {
-  ami                    = "ami-0a3c3a20c09d6f377"
+  ami                    = "ami-0e670eb768a5fc3d4"
   instance_type          = "t2.micro"
  
   tags = {
